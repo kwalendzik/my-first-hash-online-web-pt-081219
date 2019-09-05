833546c3a690423b36820ed53ebd012fb0d1106a
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-09-05 01:48:28 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -3,12 +3,11 @@ def my_hash
 end
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  the_manifest - 
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+  the_manifest = {
+ "whale bone corset" => 5,
+ "porcelain vase" => 2,
+ "oil painting" => 3
+ }
 end
 
 def retrieval
@@ -18,7 +17,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+shipping_manifest[3]
 
 end
 
