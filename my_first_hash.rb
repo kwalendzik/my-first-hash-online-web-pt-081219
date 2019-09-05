81c505eeaab3aba3482bb941872c2f2267b3260d
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-09-05 01:46:39 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,12 +1,10 @@
 def my_hash
   vacation = {"destination" => "Japan", "month" => "June"}
 end
-  
-end
-
 
 def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
+  the_manifest - 
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 
