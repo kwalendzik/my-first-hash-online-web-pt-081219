b1e0631aa80845b952d280d1b4f873d1a3f1b285
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-09-05 01:53:52 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -3,11 +3,7 @@ def my_hash
 end
 
 def shipping_manifest
-  the_manifest = {
- "whale bone corset" => 5,
- "porcelain vase" => 2,
- "oil painting" => 3
- }
+the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
